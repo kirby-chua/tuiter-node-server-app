215e6120c0adf52f6149e4736f53113bbaac8807
kirby-chua <kirbyezra@gmail.com>
2022-11-15 02:25:29 -0500

    node-server-app first commit

---
+++ controllers/hello-controller.js
@@ -0,0 +1,5 @@
+const HelloController = (app) => {
+    app.get('/hello', (req, res) => {res.send('Hello World!')})
+    app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+}
+export default HelloController
